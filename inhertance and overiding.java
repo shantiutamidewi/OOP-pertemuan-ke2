8ad0672006323c0900793cbd6a7da789ee23c637
shantiutamidewi <shantiutanidewi@gmail.com>
2017-04-09 21:57:10 +0700

    tugas inheritance and overiding

---
+++ inhertance and overiding.java
@@ -0,0 +1,41 @@
+
+
+
+public class mobil{
+	//Atribut class pada mobil{
+	String warna;
+	String merk;
+	String jenis;
+	int tahunproduksi;
+	
+	void hidupkanMesin(){
+		System.out.println("Mesin Mobil Hidup");
+	}
+	
+	void maju(){
+		System.out.println("Mobil Maju");
+	}
+        void berhenti(){
+		System.out.println("Mobil Berhenti");
+	}
+	
+}
+	public class kendaraan extend mobil
+          private String menyetir;
+          private String mengerem;
+   void menyetir (){
+          System.out.println("menyetir stang");
+
+}
+     void mengerem(){
+            System.out.println("mengerem gas");
+  }
+}
+     public class mobil{
+   public static void main (String args[]){
+      kendaraan  mobilku= new mobilku();
+           mobilku.menyetir();
+           mobilku.mengerem();
+ }
+}
+	
